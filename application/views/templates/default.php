1a91983262ac095259c2ec4bf424e3721496e73a
Nick Hentschel <hentschel.nicholas@gmail.com>
2013-02-18 16:03:11 -0500

    added css reset, make some template tweaks

--- application/views/templates/default.php
+++ application/views/templates/default.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html>
-   <head>
-      <title><?php echo $title; ?></title>
-   </head>
-   <body>
-      <h1>Default template</h1>
-      <div class="wrapper">
-         <?php echo $body; ?>
-      </div>
-   </body>
+	<head>
+		<title><?php echo $title; ?></title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="/assets/css/reset.css">
+		<link rel="stylesheet" type="text/css" href="/assets/css/style.css">		
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>		
+	</head>
+	<body>
+		<?php echo $body; ?>
+	</body>
 </html>
