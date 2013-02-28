9308f8c304cba3fe88b086325a73fa2cdf121c58
Nick Hentschel <hentschel.nicholas@gmail.com>
2013-02-28 00:40:45 -0500

    fixed input parsing from swipe and made a very small css tweak

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,5 +1,19 @@
+// reg input:   01234567
+// swipe input: ;001234567;
+
+/**
+ *
+ *	Javascript input parsing
+ *	
+ *	TODO:
+ *	Real documentation here
+ *	Clean up code - it's not very pretty or efficient
+ * 
+ * 
+ */
+
 var input = "";
-var reg   = new RegExp('^[0-9]+$');
+var reg   = new RegExp('^[0-9|;]+$');
 
 function showStar() {
 	$('.stars').append('<span class="star">*</span>');
@@ -8,22 +22,24 @@ function showStar() {
 function handleInput(e) {
 	if(reg.test(String.fromCharCode(e.which))) {
 		input += String.fromCharCode(e.which);
-		console.log(input);
-		console.log(e.which);
-		if(input.length == 8) {
+		console.log(String.fromCharCode(e.which));
+		if(input.length == 8 && input.indexOf(';') === -1) {
+			console.log(input);
+			$('.stars').empty();
+			alert("Login Successful");
+		} else if(input.length == 10) {
+			input = input.substring(2, input.length);
 			console.log(input);
 			$('.stars').empty();
 			alert("Login Successful");
 		} else {
-			//input += String.fromCharCode(e.which);
 			showStar();
 		}
-
 	}
 }
 
 $(document).ready(function(){
-	$(window).keypress(function (e) {
+	$(window).keypress(function(e) {
 		handleInput(e);
 	});
 });
