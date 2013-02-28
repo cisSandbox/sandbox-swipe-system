f75f3a540c959078238d7117310233b26893c00d
Nick Hentschel <hentschel.nicholas@gmail.com>
2013-02-28 10:56:32 -0500

    added a very simple student model

---
+++ application/models/student_model.php
@@ -0,0 +1,15 @@
+<?php 
+
+class Student_model extends CI_Model {
+
+	function get_students() {
+		$query = $this->db->get('student');
+		return $query->result();
+	}
+
+	function get_student_by_id($id) {
+		$query = $this->db->query("select * from student where studentID = $id");
+		return $query->result();	
+	}
+
+}
