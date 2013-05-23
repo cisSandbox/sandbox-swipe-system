4fd7551f9bee1d21d71361691a2d913682403922
Conner Charlebois <cdcharlebois@gmail.com>
2013-05-22 20:40:42 -0400

    added fork

---
+++ application/views/fork.php
@@ -0,0 +1,9 @@
+<div class="page-header text-center">
+	<h1>CIS Sandbox Swipe App <br><small>By Conner Charlebois and Nick Hentschel</small></h1>
+</div>
+<div class="row-fluid">
+	<a class="btn btn-primary span3" href="/index.php/main/swipe">Front Screen</a>
+	<a class="btn btn-primary span3" href="/index.php/visit/tapout_queue">Tapout Queue (Tablet near the door)</a>
+	<a class="btn btn-primary span3" href="/index.php/visit/tutor_queue">Tutor Queue (Login Required)</a>
+	<a class="btn btn-primary disabled span3">DA Console (Login Required)</a>
+</div>
